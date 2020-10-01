ef5f63c38238d411c1357ce56f139b0d650ce4b8
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-10-01 08:33:03 -0400

    done

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,7 +2,13 @@
  * YOUR CODE HERE *
  ******************/
 
-
+function isValidPrimary(color) {
+  if (color === 'red' || color === 'yellow' || color === 'blue') {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 // Our setup code here. Don't touch!
