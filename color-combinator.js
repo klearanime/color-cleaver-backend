29cf61fd41c6779059b2c246b71797e07373401a
Crystal K Young <crystalyoung@Crystals-MacBook-Pro.local>
2020-10-01 08:17:54 -0400

    done

--- color-combinator.js
+++ color-combinator.js
@@ -2,6 +2,18 @@
  * YOUR CODE HERE *
  ******************/
 
+function colorCombinator(color1, color2) {
+  if (color1 === 'red' && color2 ==='yellow' || 
+        color1 === 'yellow' && color2 === 'red') {
+    return 'orange';
+  } else if (color1 === 'red' && color2 === 'blue' || 
+              color1 === 'blue' && color2 === 'red') {
+    return 'purple';
+  } else if (color1 === 'yellow' && color2 === 'blue' || 
+              color1 === 'blue' && color2 === 'yellow') {
+    return 'green';
+  }
+}
 
 
 
